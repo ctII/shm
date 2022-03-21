a8ade58529795c20f30e08ac17727f92cfe74d91
Clayton Townsend II <25651218+ctII@users.noreply.github.com>
2022-03-21 18:50:02 -0500

    add tests for WriteAt

--- shm_test.go
+++ shm_test.go
@@ -149,3 +149,54 @@ func TestSharedMemory_Close(t *testing.T) {
 		return
 	}
 }
+
+func TestSharedMemory_WriteAt(t *testing.T) {
+	t.Parallel()
+	const size = 512
+	shm := New(0, size)
+	err := shm.Open()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer func() {
+		if err == nil {
+			return
+		}
+		err2 := shm.Close()
+		if err2 != nil {
+			err = multierror.Append(err, err2)
+			t.Error(err)
+		}
+	}()
+
+	bCopy := make([]byte, size)
+
+	for i := 0; i < size; i++ {
+		shm.b[i] = 1
+	}
+
+	_, err = shm.WriteAt(bCopy, 0)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(shm.b, bCopy) {
+		t.Error("shm: reflect.DeepEqual returned false for shm.b and bCopy")
+		return
+	}
+
+	_, err = shm.WriteAt(bCopy, size)
+	if err != ErrorGivenSliceTooBig {
+		t.Errorf("error not ErrorGivenSliceTooBig is (%v)\n", err)
+		return
+	}
+
+	err = shm.Close()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
